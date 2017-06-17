d873e81b634e53f94afd61ccd4dd43bda1f30868
christo <christo@192.168.0.3>
2017-06-17 12:25:40 +0200

    Creating default constructor and fields

--- CS61BUniversity/src/homework6dict/HashTableChained.java
+++ CS61BUniversity/src/homework6dict/HashTableChained.java
@@ -1,148 +1,167 @@
 package homework6dict;
-import list.*;
+
 /* HashTableChained.java */
+import list.List;
 
 /**
- *  HashTableChained implements a Dictionary as a hash table with chaining.
- *  All objects used as keys must have a valid hashCode() method, which is
- *  used to determine which bucket of the hash table an entry is stored in.
- *  Each object's hashCode() is presumed to return an int between
- *  Integer.MIN_VALUE and Integer.MAX_VALUE.  The HashTableChained class
- *  implements only the compression function, which maps the hash code to
- *  a bucket in the table's range.
+ * HashTableChained implements a Dictionary as a hash table with chaining. All
+ * objects used as keys must have a valid hashCode() method, which is used to
+ * determine which bucket of the hash table an entry is stored in. Each object's
+ * hashCode() is presumed to return an int between Integer.MIN_VALUE and
+ * Integer.MAX_VALUE. The HashTableChained class implements only the compression
+ * function, which maps the hash code to a bucket in the table's range.
  *
- *  DO NOT CHANGE ANY PROTOTYPES IN THIS FILE.
+ * DO NOT CHANGE ANY PROTOTYPES IN THIS FILE.
  **/
 
 public class HashTableChained implements Dictionary {
 
-  /**
-   *  Place any data fields here.
-   **/
-private int size; // # of elemnts stored in arrayBucket
-private int N;//size of Arraybucket
-private List slist;
-private List[] arrayBucket;
-private long scale;
-private long shift;
-private long prime;
+	/**
+	 * Place any data fields here.
+	 **/
+	private int size = 0; // # of elemnts stored in arrayBucket
+	private int N;// size of Arraybucket
+	private List slist;
+	private List[] arrayBucket;
+	private long scale;
+	private long shift;
+	private long prime = 109345121;
 
+	/**
+	 * Construct a new empty hash table intended to hold roughly sizeEstimate
+	 * entries. (The precise number of buckets is up to you, but we recommend
+	 * you use a prime number, and shoot for a load factor between 0.5 and 1.)
+	 **/
 
-  /** 
-   *  Construct a new empty hash table intended to hold roughly sizeEstimate
-   *  entries.  (The precise number of buckets is up to you, but we recommend
-   *  you use a prime number, and shoot for a load factor between 0.5 and 1.)
-   **/
+	public HashTableChained(int sizeEstimate) {
+		// Your solution here.
 
-  public HashTableChained(int sizeEstimate) {
-    // Your solution here.
-	  int size=0;
-  }
+	}
 
-  /** 
-   *  Construct a new empty hash table with a default size.  Say, a prime in
-   *  the neighborhood of 100.
-   **/
+	/**
+	 * Construct a new empty hash table with a default size. Say, a prime in the
+	 * neighborhood of 100.
+	 **/
 
-  public HashTableChained() {
-    // Your solution here.
-	  int size=0;
-  }
+	public HashTableChained() {
+		// Your solution here.
 
-  /**
-   *  Converts a hash code in the range Integer.MIN_VALUE...Integer.MAX_VALUE
-   *  to a value in the range 0...(size of hash table) - 1.
-   *
-   *  This function should have package protection (so we can test it), and
-   *  should be used by insert, find, and remove.
-   **/
+		N = 107;
+		arrayBucket = new List[N];
+		java.util.Random rand = new java.util.Random();
+		scale = rand.nextInt((N - 1) + 1);
+		shift = rand.nextInt(N);
+	}
 
-  int compFunction(int code) {
-    // Replace the following line with your solution.
-    return 88;
-  }
+	/**
+	 * Converts a hash code in the range Integer.MIN_VALUE...Integer.MAX_VALUE
+	 * to a value in the range 0...(size of hash table) - 1.
+	 *
+	 * This function should have package protection (so we can test it), and
+	 * should be used by insert, find, and remove.
+	 **/
 
-  /** 
-   *  Returns the number of entries stored in the dictionary.  Entries with
-   *  the same key (or even the same key and value) each still count as
-   *  a separate entry.
-   *  @return number of entries in the dictionary.
-   **/
+	int compFunction(int code) {
+		// Replace the following line with your solution.
+		return 88;
+	}
 
-  public int size() {
-    // Replace the following line with your solution.
-    return size;
-  }
+	/**
+	 * Returns the number of entries stored in the dictionary. Entries with the
+	 * same key (or even the same key and value) each still count as a separate
+	 * entry.
+	 * 
+	 * @return number of entries in the dictionary.
+	 **/
 
-  /** 
-   *  Tests if the dictionary is empty.
-   *
-   *  @return true if the dictionary has no entries; false otherwise.
-   **/
+	@Override
+	public int size() {
+		// Replace the following line with your solution.
+		return size;
+	}
 
-  public boolean isEmpty() {
-    // Replace the following line with your solution.
-    return size==0;
-  }
+	/**
+	 * Tests if the dictionary is empty.
+	 *
+	 * @return true if the dictionary has no entries; false otherwise.
+	 **/
 
-  /**
-   *  Create a new Entry object referencing the input key and associated value,
-   *  and insert the entry into the dictionary.  Return a reference to the new
-   *  entry.  Multiple entries with the same key (or even the same key and
-   *  value) can coexist in the dictionary.
-   *
-   *  This method should run in O(1) time if the number of collisions is small.
-   *
-   *  @param key the key by which the entry can be retrieved.
-   *  @param value an arbitrary object.
-   *  @return an entry containing the key and value.
-   **/
+	@Override
+	public boolean isEmpty() {
+		// Replace the following line with your solution.
+		return size == 0;
+	}
 
-  public Entry insert(Object key, Object value) {
-    // Replace the following line with your solution.
-    return null;
-  }
+	/**
+	 * Create a new Entry object referencing the input key and associated value,
+	 * and insert the entry into the dictionary. Return a reference to the new
+	 * entry. Multiple entries with the same key (or even the same key and
+	 * value) can coexist in the dictionary.
+	 *
+	 * This method should run in O(1) time if the number of collisions is small.
+	 *
+	 * @param key
+	 *            the key by which the entry can be retrieved.
+	 * @param value
+	 *            an arbitrary object.
+	 * @return an entry containing the key and value.
+	 **/
 
-  /** 
-   *  Search for an entry with the specified key.  If such an entry is found,
-   *  return it; otherwise return null.  If several entries have the specified
-   *  key, choose one arbitrarily and return it.
-   *
-   *  This method should run in O(1) time if the number of collisions is small.
-   *
-   *  @param key the search key.
-   *  @return an entry containing the key and an associated value, or null if
-   *          no entry contains the specified key.
-   **/
+	@Override
+	public Entry insert(Object key, Object value) {
+		// Replace the following line with your solution.
+		return null;
+	}
 
-  public Entry find(Object key) {
-    // Replace the following line with your solution.
-    return null;
-  }
+	/**
+	 * Search for an entry with the specified key. If such an entry is found,
+	 * return it; otherwise return null. If several entries have the specified
+	 * key, choose one arbitrarily and return it.
+	 *
+	 * This method should run in O(1) time if the number of collisions is small.
+	 *
+	 * @param key
+	 *            the search key.
+	 * @return an entry containing the key and an associated value, or null if
+	 *         no entry contains the specified key.
+	 **/
 
-  /** 
-   *  Remove an entry with the specified key.  If such an entry is found,
-   *  remove it from the table and return it; otherwise return null.
-   *  If several entries have the specified key, choose one arbitrarily, then
-   *  remove and return it.
-   *
-   *  This method should run in O(1) time if the number of collisions is small.
-   *
-   *  @param key the search key.
-   *  @return an entry containing the key and an associated value, or null if
-   *          no entry contains the specified key.
-   */
+	@Override
+	public Entry find(Object key) {
+		// Replace the following line with your solution.
+		return null;
+	}
 
-  public Entry remove(Object key) {
-    // Replace the following line with your solution.
-    return null;
-  }
+	/**
+	 * Remove an entry with the specified key. If such an entry is found, remove
+	 * it from the table and return it; otherwise return null. If several
+	 * entries have the specified key, choose one arbitrarily, then remove and
+	 * return it.
+	 *
+	 * This method should run in O(1) time if the number of collisions is small.
+	 *
+	 * @param key
+	 *            the search key.
+	 * @return an entry containing the key and an associated value, or null if
+	 *         no entry contains the specified key.
+	 */
 
-  /**
-   *  Remove all entries from the dictionary.
-   */
-  public void makeEmpty() {
-    // Your solution here.
-  }
+	@Override
+	public Entry remove(Object key) {
+		// Replace the following line with your solution.
+		return null;
+	}
+
+	/**
+	 * Remove all entries from the dictionary.
+	 */
+	@Override
+	public void makeEmpty() {
+		// Your solution here.
+	}
+
+	private int eratosthenisSieve(int capacity) {
+		return 0;
+	}
 
 }
